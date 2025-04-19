f844c3241e69bd1582c298e2a203d691e51379f5
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-04-18 23:27:05 -0400

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from movies.models import Movie
+from movies.serializers import MovieSerializer
+
+
 
 # Create your views here.
+
+class MoviesCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Movie.objects.all()
+    serializer_class = MovieSerializer
