949c49b740d9b320cbb313fe156503e7d9cb19e3
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-04-21 11:21:37 -0400

    feat: create model review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from movies.models import Movie 
 
-# Create your models here.
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie,on_delete=models.PROTECT,
+    related_name='reviews')
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(0,'Minimum value is 0.'),
+            MaxValueValidator(5,'Maximum value is 5.')
+            
+        ]
+    )
+    comment=models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.movie
