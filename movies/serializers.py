32b5292643fe284b52019cb190a761ba13a2099e
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-04-22 14:33:24 -0400

    Update serializers.py

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
+from django.db.models import Avg
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields='__all__'
+
+    def get_rate(self,obj):
+        rate = obj.reviews.agregate(Avg('stars'))['stars__avg']
+        if rate:
+            return round(rate,1)
+        return None
+        
+
+    def validate_release_date(self,value):
+        if value.year<1900:
+            raise serializers.ValidationError('Release date must be after 1990')
+        return value
+
+    def validate_resume(self,value):
+        if len(value)>1500:
+            raise serializers.ValidationError('Resume must be less than 200 characters')
+        elif len(value)<10:
+            raise serializers.ValidationError('Resume must be more than 10 characters')
+        return value
